fdbaade9cb8908003f3193d447774b3828a3ed30
esterTion <lcz970@126.com>
2025-04-24 10:54:58 +0800

    缩放

--- src/main.js
+++ src/main.js
@@ -31,7 +31,7 @@ Set.prototype.union ??= function (set) {
   const result = new Set(this);
   for (const item of set) result.add(item);
   return result;
-}
+};
 
 // createElement
 function _(e,t,i){var a=null;if("text"===e)return document.createTextNode(t);a=document.createElement(e);for(var n in t)if("style"===n)for(var o in t.style)a.style[o]=t.style[o];else if("className"===n)a.className=t[n];else if("event"===n)for(var o in t.event)a.addEventListener(o,t.event[o]);else a.setAttribute(n,t[n]);if(i)if("string"==typeof i)a.innerHTML=i;else if(Array.isArray(i))for(var l=0;l<i.length;l++)null!=i[l]&&a.appendChild(i[l]);return a;}
@@ -136,6 +136,7 @@ function init() {
   controls.enableDamping = false;
   controls.enableRotate = false;
   controls.autoRotate = false;
+  controls.zoomToCursor = true;
   controls.addEventListener('change', () => requestRender = true);
 
   initSprites();
@@ -611,7 +612,7 @@ const RewardTypes = {
   'unknown': '物品'
 };
 
-const finishedTiles = {}
+const finishedTiles = {};
 function loadStoredState() {
   const storedState = localStorage.getItem('finishedTiles');
   if (storedState) {
@@ -623,7 +624,7 @@ function loadStoredState() {
   }
 }
 function saveStoredState() {
-  const store = {}
+  const store = {};
   for (const mapId in finishedTiles) {
     if (mapId === 'current') continue;
     const set = finishedTiles[mapId];
@@ -664,7 +665,7 @@ function renderTileInfo(pos) {
         if (i && i.unlockTree.has(selectedTileId)) {
           finishedTiles.current.delete(i.TileID);
         }
-      })
+      });
     } else {
       tileInfo.unlockTree.forEach(i => finishedTiles.current.add(i));
       finishedTiles.current.add(selectedTileId);
